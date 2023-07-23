9151622e2d073d7a9b05a4881e47a96cbc360940
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-07-23 12:31:54 -0700

    python tutorial

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -6,8 +6,8 @@ word_last_2 = word[7:]
 
 middle_word = word[1:8]
 
-print("First 3 letters: {}".format(word_first_3))
+print("First 3 letters: {}".format(word[:3]))
 
-print("Last 2 letters: {}".format(word_last_2))
+print("Last 2 letters: {}".format(word[7:]))
 
-print("Middle word: {}".format(middle_word))
+print("Middle word: {}".format(word[1:8]))
