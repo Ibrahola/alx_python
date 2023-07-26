de659d84afc1e91c1efe3c5ba008d3c5f13f38b2
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-07-26 11:22:53 -0700

    python tutorial

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,4 +1,7 @@
 def reverse_string(string):
-    return reversed(string)
+    return string[::-1]
 
-print(reverse_string("hello"))
+print(reverse_string("Hello"))
+print(reverse_string(""))
+print(reverse_string("madam"))
+print(reverse_string("Hello, World!"))
