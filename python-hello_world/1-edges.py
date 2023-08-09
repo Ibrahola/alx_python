54282102a6c2214397339cd745fa4011d1ebbd46
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-08-09 00:06:10 -0700

    python tutorial

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,7 +1,7 @@
 # printing words
 word = "Holberton"
 
-print("First 3 letters: {}".format(word[:3]))
+print("First 3 letters: {}".format(word[0:3]))
 
 print("Last 2 letters: {}".format(word[7:]))
 
