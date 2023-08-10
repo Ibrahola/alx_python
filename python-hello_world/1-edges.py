66b183a5d76fffba86f8bcde0c5aef80f7a8f363
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-08-09 20:46:09 -0700

    python tutorial

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 # printing words
-word = "Holberton"
+word = "School"
 
 print("First 3 letters: {}".format(word[:3]))
 
-print("Last 2 letters: {}".format(word[7:]))
+print("Last 2 letters: {}".format(word[4:]))
 
-print("Middle word: {}".format(word[1:8]))
+print("Middle word: {}".format(word[1:5]))
