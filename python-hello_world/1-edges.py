919b87999b3b00905f34981b43f81ba6ed779562
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-08-09 21:09:45 -0700

    python tutorial

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -2,7 +2,7 @@ word = "Holberton"
 word1 = "School"
 print("First 3 letters: {}".format(word[:3]))
 print("Last 2 letters: {}".format(word[7:]))
-print("Middle word: {}".format(word[1:8]))
+print("Middle word: {}".format(word[1:7]))
 # print("First 3 letters: {}".format(word1[:3]))
 # print("Last 2 letters: {}".format(word1[4:]))
 # print("Middle word: {}".format(word1[1:5]))
