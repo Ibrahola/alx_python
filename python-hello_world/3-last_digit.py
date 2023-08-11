008d8e8fec3b5e0154a59dd5929eee4104300f0a
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-08-10 22:09:47 -0700

    python tutorial

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -8,6 +8,6 @@ if number < 0:
 elif num > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, num))
 elif num == 0:
-    print("The last digit of {} is {} and is 0".format(number, num))
+    print("Last digit of {} is {} and is 0".format(number, num))
 else:
     print("The last digit of {} is {} and it is less than 6 and not 0".format(number, num))
