51e2c81fb09d1ba8767ba07c3ac0205597ac8d6b
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-08-11 18:08:46 -0700

    python tutorial

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,5 +1,4 @@
-
-hex = "0,1,2,3,4,5,6,7,8,9,10,a,b,c,d,e,f"
+hex = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
 for number in range(99):
-    for i in hex:
-        print("{} = {} * {}".format(number, (number * i)))
+        for i in hex:
+            print("{} = {} * {}".format(number, number, i))
