c93db60e2970dc64398c4cf0dfc21eb196e82113
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-08-13 12:14:38 -0700

    python classes

---
+++ python-classes/1-square.py
@@ -0,0 +1,46 @@
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+        if size == "3":
+            print("size must be an integer")
+        elif size < 0:
+            print("size must be >= 0")
+
+
+
+my_square_1 = Square(3)
+print(type(my_square_1))
+print(my_square_1.__dict__)
+
+my_square_2 = Square()
+print(type(my_square_2))
+print(my_square_2.__dict__)
+
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_3 = Square("3")
+    print(type(my_square_3))
+    print(my_square_3.__dict__)
+except TypeError as e:
+    print("size must be an integer")
+
+try:
+    my_square_4 = Square(-89)
+    print(type(my_square_4))
+    print(my_square_4.__dict__)
+except Exception as e:
+    print(e)
+
+
+
+
+
