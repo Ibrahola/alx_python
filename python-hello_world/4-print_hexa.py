2c2e424c3f5b502f721b08dd140434074cabbd19
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-08-13 11:24:36 -0700

    python classes

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,4 +1,10 @@
-hex = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
+# hex = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
+# for number in range(99):
+#     for i in hex:
+#         print("{} = {} * {}".format(number, number, i))
+
+
 for number in range(99):
+    hex = [0, 1, 2]
     for i in hex:
         print("{} = {} * {}".format(number, number, i))
